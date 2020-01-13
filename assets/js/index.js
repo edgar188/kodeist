8c5ec8d2c0fbab24ec670268b7822f01c3f9f7de
edgar <edgar.hovhannisyan08@gmail.com>
2020-01-13 20:04:43 +0400

    created new partial files

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,15 @@
-function onInfo() {
-    var infoNode = document.getElementById("info-hidden");
+// function onInfo() {
+//     var infoNode = document.getElementById("info-hidden");
 
-    infoNode.className === "info-hidden"
-        ? infoNode.className += " info-list-open"
-        : infoNode.className = "info-hidden";
-}
+//     infoNode.className === "info-hidden"
+//         ? infoNode.className += " info-list-open"
+//         : infoNode.className = "info-hidden";
+// }
 
-function onMenu() {
-    var navsWrapper = document.getElementById('navs');
+// function onMenu() {
+//     var navsWrapper = document.getElementById('navs');
     
-    navsWrapper.className === "navs-wrapper"
-        ? navsWrapper.className += " navs-open"
-        : navsWrapper.className = "navs-wrapper";
-}
+//     navsWrapper.className === "navs-wrapper"
+//         ? navsWrapper.className += " navs-open"
+//         : navsWrapper.className = "navs-wrapper";
+// }
