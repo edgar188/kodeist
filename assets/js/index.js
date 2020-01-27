9a7d156b0b739c163101e4b13de629048cb525f8
edgar <edgar.hovhannisyan08@gmail.com>
2020-01-27 14:39:41 +0400

    The responsive settings page is already finalized.

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,17 @@
 function onNavBtn() {
     var asideNode = document.getElementById("aside");
 
-    if(asideNode.className === "sect-cont open") {
-        asideNode.className = "sect-cont close";
-    } else {
-        asideNode.className = "sect-cont open";
-    }
+    asideNode.className === "sect-cont open" 
+        ? asideNode.className = "sect-cont close"
+        : asideNode.className = "sect-cont open";
+}
+
+
+function onChangeVal(node) {
+    var label = node.nextElementSibling;
+    
+    node.value 
+        ? label.setAttribute("class", "label-active")
+        : label.removeAttribute("class");
 }
 
